d9751a975d2209102ad02d6947d336d341a00057
tyyuan <tyyuan@aibee.com>
2021-01-23 14:44:07 +0800

    add black and blank cells

--- patterns.py
+++ patterns.py
@@ -4,18 +4,32 @@ version:
 Author: TianyuYuan
 Date: 2021-01-22 22:44:59
 LastEditors: TianyuYuan
-LastEditTime: 2021-01-23 12:10:37
+LastEditTime: 2021-01-23 14:42:03
 '''
 from draw import Cell
+###### Unit #######
+WIDTH = 6
+HEIGHT = 3
+
+###### Blank ######
+BLANK_WIDTH = WIDTH
+BLANK_HEIGHT = HEIGHT
+BLANK_PATTERN = "\
+      \n\
+      \n\
+      "
+class BlankCell(Cell):
+    def __init__(self):
+        super().__init__(width=BLANK_WIDTH,height=BLANK_HEIGHT,color='k',bold=False,pattern=BLANK_PATTERN)
 
 ###### Buttons ######
-BUTTON_WIDTH = 5
-BUTTON_HEIGHT = 3
+BUTTON_WIDTH = WIDTH
+BUTTON_HEIGHT = HEIGHT
 BUTTON_BOLD = 'b'
 BUTTON = "\
-+---+\n\
-|   |\n\
-+---+"
++----+\n\
+|    |\n\
++----+"
 
 class RedCell(Cell):
     def __init__(self):
@@ -33,6 +47,9 @@ class BlueCell(Cell):
     def __init__(self):
         super().__init__(width=BUTTON_WIDTH,height=BUTTON_HEIGHT,color='b',bold=BUTTON_BOLD,pattern=BUTTON)
 
+class BlackCell(Cell):
+    def __init__(self):
+        super().__init__(width=BUTTON_WIDTH,height=BUTTON_HEIGHT,color='k',bold=BUTTON_BOLD,pattern=BUTTON)
 
-pc = PinkCell()
-pc.show_cell()
+# pc = PinkCell()
+# pc.show_cell()
