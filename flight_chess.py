5edd1850dac7377049687007dbc86a51d4a35069
tyyuan <tyyuan@aibee.com>
2021-01-26 19:53:29 +0800

    add some plan

--- flight_chess.py
+++ flight_chess.py
@@ -8,5 +8,36 @@ LastEditTime: 2021-01-20 22:40:54
 '''
 
 
+def play():
+
+
+
+def turn(player):
+    '''在一回合内，player的所有行为'''
+    # 投骰子
+    # 显示骰子
+    # 有可移动的吗？
+    if 没有可移动的
+        # 打印信息
+        # 下一回合
+    else 有可移动的
+        while True：
+        # 询问移动哪个
+        # 用户输入
+        # 输入检测，是否合法
+        if 不合法
+            # 报错提示
+            continue
+        else 合法
+            # 棋子移动
+            # 输赢判断
+            if 一位玩家全部棋子到达
+                # 游戏结束，打印结果
+            else
+                # 下一回合
+                    
+def run():
+
+
 if __name__ == "__main__":
     run()
