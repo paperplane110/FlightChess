c58effd002a82e928150a9bd3d40cd39c60db793
tyyuan <tyyuan@aibee.com>
2021-01-31 23:30:20 +0800

    fix move plane

--- player.py
+++ player.py
@@ -4,13 +4,13 @@ version:
 Author: TianyuYuan
 Date: 2021-01-31 18:21:40
 LastEditors: TianyuYuan
-LastEditTime: 2021-01-31 21:08:05
+LastEditTime: 2021-01-31 21:19:27
 '''
 from plane import Plane
 from dice import Dice
 
 class Player():
-    '''玩家类：包括玩家的初始化，掷骰子，决定移动哪个棋子，输赢判断等'''
+    '''玩家类：包括玩家的初始化，掷骰子，决定移动哪个棋子'''
     def __init__(self,color:str):
         self.color = color
         self.planes = self.init_planes()
@@ -31,10 +31,22 @@ class Player():
         '''行为：掷骰子'''
         self.dice.roll_dice()
 
-    def move_plane(self,index:int):
-        '''行为：移动飞机'''
+    def move_plane(self,index:int) -> bool:
+        '''
+        行为：移动飞机
+        + index: 飞机的序号
+        + return: 是否移动成功
+        '''
         num = self.dice.point
         plane = self.planes[index-1]
-        plane.animate(num)
+        # check if the plane is in the airport
+        if plane.in_airport:
+            if num == 6:
+                plane.animate(1)
+            else:
+                return False
+        else:
+            plane.animate(num)
+        return True
 
 
